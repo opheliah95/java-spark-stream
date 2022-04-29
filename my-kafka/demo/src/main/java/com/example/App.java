77b4c5b06d46996a37c98b76aca762f1834cdcd2
opheliah95 <opheliah95@gmail.com>
2022-04-29 17:04:39 +0100

    initial spark setup

---
+++ my-kafka/demo/src/main/java/com/example/App.java
@@ -0,0 +1,24 @@
+package com.example;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.*;
+import org.apache.spark.api.java.function.*;
+import org.apache.spark.streaming.*;
+import org.apache.spark.streaming.api.java.*;
+import scala.Tuple2;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // set logger so no messy messages
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+        
+        // Create a local StreamingContext with two working thread and batch interval of 1 second
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        // use spark streaming
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost",9999);
+    }
+}
