7496a7274feb26d5e698eca576498358eca27e6d
opheliah95 <opheliah95@gmail.com>
2022-04-29 17:20:35 +0100

    updated maven build path

--- my-kafka/demo/src/main/java/com/example/App.java
+++ my-kafka/demo/src/main/java/com/example/App.java
@@ -1,4 +1,7 @@
 package com.example;
+
+import java.util.Arrays;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.*;
@@ -7,18 +10,20 @@ import org.apache.spark.streaming.*;
 import org.apache.spark.streaming.api.java.*;
 import scala.Tuple2;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         // set logger so no messy messages
         Logger.getLogger("org.apache").setLevel(Level.WARN);
-        
-        // Create a local StreamingContext with two working thread and batch interval of 1 second
+
+        // Create a local StreamingContext with two working thread and batch interval of
+        // 1 second
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
 
         // use spark streaming
-        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost",9999);
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
     }
 }
